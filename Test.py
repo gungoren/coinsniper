9764174e8c157e66928b53eee20db9ccb53cf44b
Mehmet Güngören <mehgungoren@gmail.com>
2018-06-14 00:05:35 +0300

    binance listing token notifier over telegram

--- Test.py
+++ Test.py
@@ -1,11 +1,10 @@
 from telethon import TelegramClient
-
-# These example values won't work. You must get your own api_id and
-# api_hash from https://my.telegram.org, under API Development.
 from telethon.tl.types import PeerChannel
 import local
 import re
 from db import Database
+import requests
+from bs4 import BeautifulSoup
 
 database = Database()
 client = TelegramClient('session_name', local.api_id, local.api_hash)
@@ -49,3 +48,23 @@ while True:
         database.insert(add_notification, notification)
 
 database.close()
+
+# send messages related binance listing
+channel = PeerChannel(channel_id=1347367599)
+
+last_message = client.get_messages(channel, limit=1)[0]
+
+resp = requests.get(local.listing_page)
+
+soup = BeautifulSoup(resp.text, "html5lib")
+links = soup.find_all('li', class_='article-list-item')
+
+my_list = list()
+for link in links:
+    if last_message.message != link.text.strip():
+        my_list.append(link.text.strip())
+    else:
+        break
+my_list.reverse()
+for l in my_list:
+    client.send_message(channel, l)
