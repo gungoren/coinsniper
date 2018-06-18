a67043b44dca281a7eff6f88304f81d08051d3cd
Mehmet Güngören <mehgungoren@gmail.com>
2018-06-19 02:34:14 +0300

    .

--- local_.py
+++
@@ -1,7 +0,0 @@
-api_id = 214220
-api_hash = '511201d26301aacd9da82406f457922f'
-host = 'localhost'
-user = 'root'
-password = '1qaz2wsX'
-db = 'trading'
-listing_page = 'https://support.binance.com/hc/en-us/sections/115000106672-New-Listings'
