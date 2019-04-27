6917a8f0d4fd7a295ce70fb125bde6a3842f5079
Mehmet Güngören <mehgungoren@gmail.com>
2019-04-27 11:34:44 +0300

    query from log table and publish close message

--- supertrend.py
+++ supertrend.py
@@ -18,10 +18,13 @@ class SuperTrend:
 
         last_id = last_message.message.split(':')[0]
 
-        cursor = database.query("select id, currency, enter_price from orders_new where id > " + str(last_id))
+        cursor = database.query("select id, order_id, action, currency, enter_price, exit_price from orders_new_log where id > " + str(last_id))
         for row in cursor:
-            client.send_message(channel, str(row[0]) + ":" + row[1] + " enter at " + str(row[2]))
+            if row[2] == 'I':
+                client.send_message(channel, str(row[0]) + ":" + str(row[1]) + " " + row[3] + " enter at " + str(row[4]))
 
+            if row[2] == 'U':
+                client.send_message(channel, str(row[0]) + ":" + str(row[1]) + " " + row[3] + " close at " + str(row[5]) + " profit " + str((row[5] - row[4]) / row[4] * 100))
 
 if __name__ == "__main__":
     superTrend = SuperTrend()
